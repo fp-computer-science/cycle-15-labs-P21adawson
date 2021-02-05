c7f1a162978240127a1a645e4a578a7dd9bc303f
P21adawson <70960562+P21adawson@users.noreply.github.com>
2021-02-05 08:29:18 -0500

    Create lab_1-1.py

---
+++ lab_1-1.py
@@ -0,0 +1,6 @@
+#AZAAN dawson amdg
+
+weekdays = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+weekends = ("Saturday", "Sunday")
+days_of_the_week = weekdays + weekends
+print(days_of_the_week)
